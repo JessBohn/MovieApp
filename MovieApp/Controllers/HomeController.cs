09cc9bfa3c13b764fd681f07c463552b51ec722c
Jessica Bohn <bohn.16@wright.edu>
2018-02-26 02:27:54 -0600

    Unlike create, the ID is needed to pass through Edit so that the proper item is identified and changed in the DB

--- MovieApp/Controllers/HomeController.cs
+++ MovieApp/Controllers/HomeController.cs
@@ -79,7 +79,7 @@ namespace MovieApp.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Title,Director,DateReleased")] Movie movie)
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,Director,DateReleased")] Movie movie)
         {
             if (ModelState.IsValid)
             {
